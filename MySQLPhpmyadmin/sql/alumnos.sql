13541d1ffb6e58cdc1df1ba60ed252c150173e51
Javier Terán González <javiteran@hotmail.com>
2022-03-07 17:41:48 +0000

    use database

--- MySQLPhpmyadmin/sql/alumnos.sql
+++ MySQLPhpmyadmin/sql/alumnos.sql
@@ -1,5 +1,6 @@
 /* Consulta Apellidos, nombre y CorreoEmpresarial de la tabla Alumnos. 
     Sólo saca los 10 primeros registros de la tabla ordenados por Apellidos, Nombre */
+use Instituto;
 SELECT
     Apellidos, Nombre, CorreoEmpresarial 
 FROM Alumnos 
