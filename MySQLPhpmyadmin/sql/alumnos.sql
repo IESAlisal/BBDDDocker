305afecf4dd6da9290ca66b3f7331f89da18ec94
Javier Terán González <javiteran@hotmail.com>
2022-03-07 16:37:30 +0000

    alumnos bonito

--- MySQLPhpmyadmin/sql/alumnos.sql
+++ MySQLPhpmyadmin/sql/alumnos.sql
@@ -1,1 +1,7 @@
-select Apellidos, Nombre, CorreoEmpresarial from Alumnos Limit 10;
+/* Consulta Apellidos, nombre y CorreoEmpresarial de la tabla Alumnos. 
+    Sólo saca los 10 primeros registros de la tabla ordenados por Apellidos, Nombre */
+SELECT
+    Apellidos, Nombre, CorreoEmpresarial 
+FROM Alumnos 
+ORDER BY Apellidos, Nombre
+Limit 10;
