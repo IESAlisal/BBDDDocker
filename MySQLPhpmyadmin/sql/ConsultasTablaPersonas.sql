e1189187b7b392e0b84ec71733aae893e1258c4a
Javier Terán González <javiteran@hotmail.com>
2022-03-09 13:52:02 +0000

    Nuevos archivos sql Clientes, Personas

---
+++ MySQLPhpmyadmin/sql/ConsultasTablaPersonas.sql
@@ -0,0 +1,8 @@
+SELECT `IdPersona`,`Nombre` FROM `Personas`;
+
+select 
+    * 
+from Personas 
+order by Nombre;
+
+select * from Personas limit 2;
---
+++ MySQLPhpmyadmin/sql/ConsultasTableClientes.sql
@@ -0,0 +1,1 @@
+select name, phone from Clientes;
